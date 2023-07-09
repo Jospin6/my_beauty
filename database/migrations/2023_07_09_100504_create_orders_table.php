80728280cde084295e53f6bbdf0c747693ec5487
jospin6 <jospinndagano1@gmail.com>
2023-07-09 12:34:59 +0200

    create tables order, order_item, products and reviews and their migrations

---
+++ database/migrations/2023_07_09_100504_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('users_id')->nullable();
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->unsignedBigInteger('products_id')->nullable();
+            $table->foreign('products_id')->references('id')->on('products');
+            $table->integer("quantity");
+            $table->integer("totalprice");
+            $table->integer('statut');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
